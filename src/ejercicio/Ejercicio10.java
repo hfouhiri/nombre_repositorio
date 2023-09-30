218e4f31dd2fd80aa163e9f62e90aa62e1d35d44
hfouhiri <146168206+hfouhiri@users.noreply.github.com>
2023-09-30 18:59:56 +0200

    Add files via upload

--- src/ejercicio/Ejercicio10.java
+++ src/ejercicio/Ejercicio10.java
@@ -1,10 +1,23 @@
 package ejercicio;
-
+import java.util.Scanner;
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		        Scanner sc = new Scanner(System.in);
+
+		        // Solicita al usuario la longitud del lanzamiento en metros
+		        System.out.print("Introduce la longitud del lanzamiento en metros: ");
+		        double longitudMetros = sc.nextDouble();
+
+		        // Convierte la longitud a centímetros y obtén la parte entera
+		        int longitudCentimetros = (int) (longitudMetros * 100);
+
+		        // Muestra la parte entera en centímetros
+		        System.out.println("La longitud en centímetros es: " + longitudCentimetros + " cm");
+
+		        // Cierra el escáner
+		        sc.close();
+		    }
 
 	}
-
-}
